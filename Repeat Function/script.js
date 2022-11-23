32f3f97485c0dfa223e73572f0e0b9f75fb5ff41
Rufat Gozalov <108391767+Rufat767@users.noreply.github.com>
2022-11-23 20:58:53 +0400

    Add files via upload

---
+++ Repeat Function/script.js
@@ -0,0 +1,55 @@
+// let num1 = document.querySelector('#number1');
+// let num2 = document.querySelector('#number2');
+// let opt = document.querySelector('#sec');
+// let btn = document.querySelector('button');
+// let cvb = document.querySelector('#cavab');
+
+
+// btn.addEventListener('click', (e)=>{
+//     cvb.innerHTML = eval(number1.value + sec.value + number2.value );
+// })
+
+
+
+
+// function regem(num){
+//     let inp = document.querySelector("input").value
+//     // console.log(inp);
+//     let i = " ";
+//         if (num%2 == 0) {
+//            return "even";
+//         }
+//         else( ){
+//             return "odd";
+//                 }
+//     }
+
+
+
+
+// function years(){
+//     let inp = document.querySelector("#age").value;
+//     if( year <=20){
+//         alert('yawiniz catmir')
+//     }
+//     // else(inp.value>20)
+//     // alert("ok")
+
+// }
+
+
+
+let yaw = prompt("Sizin nece yawiniz var?");
+
+ if(i==20 , i>20) {
+    alert('Xow gelmisiniz');
+    document.querySelector('#ekran').innerHTML="Xow gelmisiniz";
+    }else if (i==19 , i ==18) {
+    alert('biraz sebr ele');
+ }
+//  else(){
+//     alert("biraz sebr ele");
+//  }
+
+
+
